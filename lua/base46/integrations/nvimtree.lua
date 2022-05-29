d814b4f372ebb600aaf821b160e6c5a8ceca6b00
Can Kolay <cankolay3499@gmail.com>
2022-05-29 11:09:33 +0300

    chore(integrations): improve nvimtree colors

--- lua/base46/integrations/nvimtree.lua
+++ lua/base46/integrations/nvimtree.lua
@@ -1,15 +1,15 @@
 local colors = require('base46').get_colors('base_30')
 
 return {
-  NvimTreeEmptyFolderName = { fg = colors.folder_bg },
+  NvimTreeEmptyFolderName = { fg = colors.blue },
   NvimTreeEndOfBuffer = { fg = colors.darker_black },
-  NvimTreeFolderIcon = { fg = colors.folder_bg },
-  NvimTreeFolderName = { fg = colors.folder_bg },
+  NvimTreeFolderIcon = { fg = colors.blue },
+  NvimTreeFolderName = { fg = colors.blue },
   NvimTreeGitDirty = { fg = colors.red },
   NvimTreeIndentMarker = { fg = colors.grey_fg },
   NvimTreeNormal = { bg = colors.darker_black },
   NvimTreeNormalNC = { bg = colors.darker_black },
-  NvimTreeOpenedFolderName = { fg = colors.folder_bg, bold = true },
+  NvimTreeOpenedFolderName = { fg = colors.blue, bold = true },
   NvimTreeGitIgnored = { fg = colors.light_grey },
   NvimTreeWinSeparator = { fg = colors.darker_black, bg = colors.darker_black },
   NvimTreeWindowPicker = { fg = colors.red, bg = colors.black2 },
