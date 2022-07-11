622602f3c785c6b16fba30fa809bbfdde99f231f
tinytub <tinytub@users.noreply.github.com>
2022-07-11 04:03:25 +0000

    chore: format with stylua

--- lua/base46/integrations/blankline.lua
+++ lua/base46/integrations/blankline.lua
@@ -1,8 +1,8 @@
 local colors = require('base46').get_colors('base_30')
 
 return {
-    IndentBlanklineChar = { fg = colors.line },
-    IndentBlanklineSpaceChar = { fg = colors.line },
-    IndentBlanklineContextChar = { fg = colors.grey },
-    IndentBlanklineContextStart = { bg = colors.black2 },
+  IndentBlanklineChar = { fg = colors.line },
+  IndentBlanklineSpaceChar = { fg = colors.line },
+  IndentBlanklineContextChar = { fg = colors.grey },
+  IndentBlanklineContextStart = { bg = colors.black2 },
 }
--- lua/base46/integrations/init.lua
+++ lua/base46/integrations/init.lua
@@ -3,51 +3,51 @@ local utils = require('base46.utils')
 local highlights = {}
 
 for _, file in pairs({
-    'default',
-    'misc',
-    'syntax',
+  'default',
+  'misc',
+  'syntax',
 }) do
-    local ok, highlight = utils.req('base46.integrations.' .. file)
-    if ok then
-        highlights = utils.merge(highlights, highlight)
-    end
+  local ok, highlight = utils.req('base46.integrations.' .. file)
+  if ok then
+    highlights = utils.merge(highlights, highlight)
+  end
 end
 
 if base46_config.load_all_highlights then
-    for _, file in pairs({
-        'bufferline',
-        'cmp',
-        'devicons',
-        'git',
-        'feline',
-        'default',
-        'misc',
-        'nvimtree',
-        'syntax',
-        'telescope',
-        'treesitter',
-        'blankline',
-        'lsp',
-        'gps',
-        'navic',
-        'whichkey',
-    }) do
-        local ok, highlight = utils.req('base46.highlights.' .. file)
-        if ok then
-            highlights = utils.merge(highlights, highlight)
-        end
+  for _, file in pairs({
+    'bufferline',
+    'cmp',
+    'devicons',
+    'git',
+    'feline',
+    'default',
+    'misc',
+    'nvimtree',
+    'syntax',
+    'telescope',
+    'treesitter',
+    'blankline',
+    'lsp',
+    'gps',
+    'navic',
+    'whichkey',
+  }) do
+    local ok, highlight = utils.req('base46.highlights.' .. file)
+    if ok then
+      highlights = utils.merge(highlights, highlight)
     end
+  end
 end
 
 local polish_hl = require('base46').get_colors('polish_hl')
 if polish_hl then
-    highlights = utils.merge(highlights, polish_hl)
+  highlights = utils.merge(highlights, polish_hl)
 end
 
 if base46_config.custom_highlights then
-    highlights = utils.merge(highlights, base46_config.custom_highlights or {})
+  highlights = utils.merge(highlights, base46_config.custom_highlights or {})
 end
 
 for hl, col in pairs(highlights) do
-    vim.api.nvim_set_hl(0, hl, col)
+  vim.api.nvim_set_hl(0, hl, col)
 end
